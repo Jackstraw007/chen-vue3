a4530fb8b40fa283a693f01ed3a82e16bbf6d84c
Jackstraw007 <cw984359007@163.com>
2022-09-27 23:07:51 +0800

    ADD: test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      counter1: 0,
+      counter2: 0,
+      timer1: "",
+      timer2: "",
+    };
+  },
   components: {
     HelloWorld
-  }
-}
+  },
+  mounted() {
+    this.timer1 = setInterval(() => {
+      this.counter1++;
+    }, 1000);
+    this.timer2 = setInterval(() => {
+      this.counter2++;
+    }, 1000);
+  },
+  methods: {
+    stopTimer() {
+      clearInterval(this.timer1);
+    },
+  },
+};
 </script>
 
 <style>
